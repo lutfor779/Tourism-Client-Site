2d10d8158c197864c30fae91ca652cd2c7a89a54
lutfor779 <mdlrrahman779@gmail.com>
2021-10-31 03:42:19 +0600

    home added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Button, Row } from 'react-bootstrap';
+import { Button, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 import HomeBanner from '../HomeBanner/HomeBanner';
@@ -37,6 +37,34 @@ const Home = () => {
 
                 <Link to="/places"><Button variant="outline-primary my-5 " >See More</Button></Link>
             </div>
+
+            <div className="my-5 p-5 bg-primary bg-opacity-25 ">
+                <Container>
+                    <h1>What Customer Say</h1>
+                    <br />
+                    <h4>"Let's start your journey with us, your dream will come true. <br /> It's really enjoyable and you will gain a lot of good memory. <br /> You will enjoy and love the trip and you will never get boor.</h4>
+                </Container>
+            </div>
+
+            <div className="my-5 p-5 ">
+                <Container>
+                    <img src="https://www.dontpayfull.com/blog/wp-content/uploads/2016/10/How-to-Travel-the-World-for-Free.gif" alt="travell" className="img-fluid" />
+                </Container>
+            </div>
+
+            <div className="mt-5 p-5 bg-success bg-opacity-25 ">
+                <Container>
+                    <h1>Why choose us?</h1>
+                    <br />
+                    <h3>We provide best service for all. <br />
+                        wE provide safe and reliable journey. <br />
+                        We are very familiar to our customers. <br />
+                        We have so many workers for provide you all kind of service. <br />
+                        We provide 24/7 support.</h3>
+
+                </Container>
+            </div>
+
 
         </div>
     );
